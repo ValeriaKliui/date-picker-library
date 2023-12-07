37702abac4c6588b643fa439187ee90fdc2d7e3c
Valeria Kliui <valeriakliui@gmail.com>
2023-12-07 17:25:20 +0300

    fix: refactor roll up config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,12 @@
-import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import terser from "@rollup/plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import svg from "rollup-plugin-svg";
 import packageJson from "./package.json" assert { type: "json" };
 import { babel } from "@rollup/plugin-babel";
-import svg from "rollup-plugin-svg";
 
 export default [
   {
@@ -22,35 +24,43 @@ export default [
       },
     ],
     plugins: [
+      svg(),
       babel({
         exclude: "node_modules/**",
         presets: ["@babel/preset-react"],
         babelHelpers: "bundled",
       }),
-      resolve({
-        extensions: [
-          ".mjs",
-          ".js",
-          ".json",
-          ".node",
-          ".jsx",
-          ".tsx",
-          ".ts",
-          ".svg",
-          ".woff",
-        ],
-      }),
+      peerDepsExternal(),
+      resolve(),
       commonjs(),
       typescript({
         tsconfig: "./tsconfig.json",
-        exclude: ["**/*.test.tsx", "**/*.test.ts", "**/*.stories.ts"],
       }),
+      terser(),
+      // resolve({
+      //   extensions: [
+      //     ".mjs",
+      //     ".js",
+      //     ".json",
+      //     ".node",
+      //     ".jsx",
+      //     ".tsx",
+      //     ".ts",
+      //     ".svg",
+      //     ".woff",
+      //   ],
+      // }),
+      // commonjs(),
+      // typescript({
+      //   tsconfig: "./tsconfig.json",
+      //   exclude: ["**/*.test.tsx", "**/*.test.ts", "**/*.stories.ts"],
+      // }),
     ],
     external: ["react", "react-dom", "styled-components"],
   },
   {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
+    input: "src/index.ts",
+    output: [{ file: packageJson.types, format: "es" }],
+    plugins: [dts.default()],
   },
 ];
